80a05df375be7f5dfe2492fb294fce7ba59fa8a0
Spike Grobstein <spikegrobstein@mac.com>
2012-03-09 08:45:49 -0500

    converted tabs to spaces

--- app/models/ipv4_address.rb
+++ app/models/ipv4_address.rb
@@ -16,23 +16,23 @@ class Ipv4Address < ActiveRecord::Base
   # int to octet conversion
   class << self
     def int2ip(int)
-    	tmp = int.to_i
-  		parts = []
+      tmp = int.to_i
+      parts = []
 
-  		3.times do |i|
-  			tmp = tmp / 256.0
-  			parts << (256 * (tmp - tmp.to_i)).to_i
-  		end
+      3.times do |i|
+        tmp = tmp / 256.0
+        parts << (256 * (tmp - tmp.to_i)).to_i
+      end
 
-  		parts << tmp.to_i
-  		parts.reverse.join('.')
+      parts << tmp.to_i
+      parts.reverse.join('.')
     end
     
     def ip2int(ip)
       return 0 unless ip =~ /\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/
 
-  		v = ip.split('.').collect { |i| i.to_i }
-  		return (v[0] << 24) | (v[1] << 16) | (v[2] << 8 ) | (v[3]);
+      v = ip.split('.').collect { |i| i.to_i }
+      return (v[0] << 24) | (v[1] << 16) | (v[2] << 8 ) | (v[3]);
     end
   end
   
